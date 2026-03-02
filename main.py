b8994245e7356d8a4d0e9c8c0c9d1c3462a3383c
Berkan Taştan <79166913+berkantastann@users.noreply.github.com>
2026-03-02 23:13:32 +0300

    team color detect added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import Tracker
+import cv2 as cv
 
 
 def main():
@@ -8,6 +9,18 @@ def main():
     tracker = Tracker("models/best.pt")
     
     tracks = tracker.get_objects_tracks(video_frames, read_from_stub=True, stub_path="stubs/tracks.pkl")
+    
+    for track_id,player in tracks["players"][0].items():
+        
+        bbox = player["bbox"]
+        frame = video_frames[0]
+        
+        cropped_player = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+        
+        cv.imwrite(f"output_videos/cropped_player_{track_id}.jpg", cropped_player)
+        
+        break
+        
     
     output_frames = tracker.draw_anotation(video_frames, tracks)
 
